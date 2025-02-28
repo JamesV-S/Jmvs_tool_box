a2f51aecb6c665b759dc52a4da868b389bb69c3e
JamesV-S <hamzvilelas@gmail.com>
2025-02-28 14:35:29 +0000

    MVC testing
    I can't seem to get the user inputs to be recognised by the controller

--- controllers/geoDB_controllers/export_database_controller.py
+++ controllers/geoDB_controllers/export_database_controller.py
@@ -30,15 +30,20 @@ class exportDatabaseController:
         self.view.presetPath_radioBtn.clicked.connect(self.sigFunc_presetPath_radioBtn)
         self.view.folderPath_text.clicked.connect(self.sigFunc_selectFolder)
         self.view.export_btn.clicked.connect(self.sigFunc_export_database)
-
+        
+        print(f"Export DB CONTROLLER after")
 
     def sigFunc_fileName(self, text):
+        print("fileName text triggured")
+        '''
+        # Model has the file name now. 
         self.model.file_name = str(self.view.fileName_text.text())
         print(f"FileName = `{self.model.file_name}`")
-        return self.model.file_name
-    
+        '''
 
     def sigFunc_presetPath_radioBtn(self):
+        print("radioBtn triggured")
+        '''
         if self.view.presetPath_radioBtn.isChecked():
             print("radio button clicked ON")
             self.view.folderPath_text.setText("...\\Jmvs_ToolBox\\databases\\geo_databases")
@@ -51,17 +56,22 @@ class exportDatabaseController:
             print("radio button clicked OFF")
             self.view.folderPath_text.setText("Select Path")
             self.view.folderPath_text.setEnabled(True)
-
+        '''
 
     def sigFunc_selectFolder(self):
+        print("selectFolder triggured")
+        '''
         if not self.view.presetPath_radioBtn.isChecked():
             directory = QtWidgets.QFileDialog.getExistingDirectory(self.view, "Select Directory")
             if directory:
                 self.view.folderPath_text.setText(directory)
                 self.model.directory = directory
-
+        '''
 
     def sigFunc_export_database(self):
+        print("export_databas triggured")
+        '''
         self.model.create_database()
         # Emit the signal to update the ComboBox function on the main geo ui (VIEW)
         self.view.databaseCreated.emit()
+        '''
